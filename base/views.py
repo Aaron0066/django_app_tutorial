c3ca7d996f65d14be32159e4a84abfdf3188ac69
aaronsowah43@gmail.com <aaronsowah43@gmail.com>
2025-03-10 02:27:24 -0400

    user logout 2:41:38

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -37,6 +38,12 @@ def loginPage(request):
     context = {}
     return render(request, 'base/login_register.html', context)
 
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     
@@ -52,11 +59,16 @@ def home(request):
     context = {'rooms': rooms, 'topics': topics, 'room_count' :rooms_count}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room' : room}
     return render(request, 'base/room.html', context)
 
+
+
+
+@login_required(login_required='/login')
 def createRoom(request):
     form = RoomForm()
 
